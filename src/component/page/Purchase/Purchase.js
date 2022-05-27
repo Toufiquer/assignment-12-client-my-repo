601148db8738e8192da80fdc7591aceb3428420e
Toufiquer Tahman <toufiquerabc@gmail.com>
2022-05-27 20:15:05 +0600

    puechase

--- src/component/page/Purchase/Purchase.js
+++ src/component/page/Purchase/Purchase.js
@@ -1,7 +1,198 @@
 import React from "react";
-
+import { useForm } from "react-hook-form";
 const Purchase = () => {
-    return <div>purchase</div>;
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm();
+    const onSubmit = data => console.log(data);
+    return (
+        <div className="my-16 container mx-auto">
+            <div>
+                <div className="m-2">
+                    <div class="hero bg-base-200">
+                        <div class="hero-content flex-col lg:flex-row">
+                            <img
+                                src="https://api.lorem.space/image/movie?w=260&h=400"
+                                class="max-w-sm rounded-lg shadow-2xl"
+                                alt="Name"
+                            />
+                            <div className="flex">
+                                <div className="text-left">
+                                    <h1 class="text-5xl font-bold">
+                                        Box Office News!
+                                    </h1>
+                                    <p class="py-6">
+                                        Provident cupiditate voluptatem et in.
+                                        Quaerat fugiat ut assumenda excepturi
+                                        exercitationem quasi. In deleniti eaque
+                                        aut repudiandae et a id nisi.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="w-full my-1 lg:w-1/2 m-12 mx-auto border py-6 px-1">
+                    <div className="text-center text-2xl my-4">
+                        Please fill the Information.
+                    </div>
+                    <form className="" onSubmit={handleSubmit(onSubmit)}>
+                        {/* Input Quantity */}
+                        <label class="label">
+                            <span class="label-text">Quantity:</span>
+                        </label>
+                        <input
+                            type="number"
+                            autoComplete="quantity"
+                            {...register("quantity", {
+                                required: {
+                                    value: true,
+                                    message: "Quantity is Required.",
+                                },
+                            })}
+                            placeholder="Select Quantity"
+                            className="input input-bordered input-accent w-full my-1"
+                        />
+                        {errors.name?.type === "required" && (
+                            <span className="label-text-alt text-lg text-red-500">
+                                {errors.name.message}{" "}
+                            </span>
+                        )}
+                        {/* --- --- --- */}
+
+                        {/* Input name */}
+                        <label class="label">
+                            <span class="label-text">Name:</span>
+                        </label>
+                        <input
+                            type="name"
+                            autoComplete="name"
+                            {...register("name", {
+                                required: {
+                                    value: true,
+                                    message: "Name is Required.",
+                                },
+                            })}
+                            placeholder="Your Name"
+                            className="input input-bordered input-accent w-full my-1"
+                        />
+                        {errors.name?.type === "required" && (
+                            <span className="label-text-alt text-lg text-red-500">
+                                {errors.name.message}{" "}
+                            </span>
+                        )}
+                        {/* --- --- --- */}
+
+                        {/* Input Email */}
+                        <label class="label">
+                            <span class="label-text">Email:</span>
+                        </label>
+                        <input
+                            type="email"
+                            autoComplete="email"
+                            {...register("email", {
+                                required: {
+                                    value: true,
+                                    message: "Email is Required.",
+                                },
+                                pattern: {
+                                    value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                                    message: "Provide a valid Email.",
+                                },
+                            })}
+                            placeholder="Your Email"
+                            className="input input-bordered input-accent w-full my-1"
+                        />
+                        {errors.email?.type === "required" && (
+                            <span className="label-text-alt text-lg text-red-500">
+                                {errors.email.message}{" "}
+                            </span>
+                        )}
+                        {errors.email?.type === "pattern" && (
+                            <span className="label-text-alt text-lg text-red-500">
+                                {errors.email.message}{" "}
+                            </span>
+                        )}
+                        {/* --- --- --- */}
+
+                        {/* Input Address */}
+                        <label class="label">
+                            <span class="label-text">Address:</span>
+                        </label>
+                        <input
+                            type="address"
+                            autoComplete="address"
+                            {...register("address", {
+                                required: {
+                                    value: true,
+                                    message: "Address is Required.",
+                                },
+                            })}
+                            placeholder="Your Address"
+                            className="input input-bordered input-accent w-full my-1"
+                        />
+                        {errors.address?.type === "required" && (
+                            <span className="label-text-alt text-lg text-red-500">
+                                {errors.address.message}{" "}
+                            </span>
+                        )}
+                        {/* --- --- --- */}
+                        {/* Input Number */}
+                        <label class="label">
+                            <span class="label-text">Mobile Number:</span>
+                        </label>
+                        <input
+                            type="text"
+                            autoComplete="number"
+                            {...register("number", {
+                                required: {
+                                    value: true,
+                                    message: "Mobile Number is Required.",
+                                },
+                                pattern: {
+                                    value: /[0-9]{11,13}/,
+                                    message: "Provide a valid Phone Number.",
+                                },
+                            })}
+                            placeholder="Your Mobile Number"
+                            className="input input-bordered input-accent w-full my-1"
+                        />
+                        {errors.number?.type === "required" && (
+                            <span className="label-text-alt text-lg text-red-500">
+                                {errors.number.message}{" "}
+                            </span>
+                        )}
+                        {/* --- --- --- */}
+                        {/* Input message */}
+                        <label class="label">
+                            <span class="label-text">Message:</span>
+                        </label>
+                        <textarea
+                            type="message"
+                            autoComplete="message"
+                            {...register("message", {
+                                required: {
+                                    value: false,
+                                },
+                            })}
+                            placeholder="Your Message"
+                            className="textarea textarea-info w-full my-1"
+                        />
+                        {/* --- --- --- */}
+
+                        <input
+                            className="btn btn-active btn-ghost block mx-auto"
+                            type="submit"
+                            value="Purchase"
+                        />
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Purchase;
